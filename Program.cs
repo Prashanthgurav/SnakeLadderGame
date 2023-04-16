b79d60adddc54ee276c4d37e410363cbf2b4626b
prashantgurav <guravp@gmail.com>
2023-04-17 01:03:40 +0530

    [Prashanth] Snakelader Game

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int WinPosition = 100;
+            int position = 0;
+
+            Random random = new Random();
+
+            while (position < WinPosition)
+            {
+                
+                int dieRoll = random.Next(1, 7);
+
+                //Player checks for an option
+                int option = random.Next(0, 3);
+
+                switch (option)
+                {
+                    case 0:
+                        // No Play
+                        break;
+                    case 1:
+                        // Ladder
+                        position += dieRoll;
+                        break;
+                    case 2:
+                        // Snake
+                        position -= dieRoll;
+                        break;
+                }
+
+                // Ensure position is within bounds
+                position = Math.Max(0, position);
+                position = Math.Min(WinPosition, position);
+
+                Console.WriteLine($"Rolled a {dieRoll}, landed on {position}");
+            }
+
+            Console.WriteLine("You won!");
+
+            // Reset game if player wants to play again
+            Console.WriteLine("Do you want to play again? (y/n)");
+            string answer = Console.ReadLine();
+            if (answer.ToLower() == "y")
+            {
+                Main(args); // Recursive call to start a new game
+            }
+            Console.ReadLine();
+        }
+    }
+}
